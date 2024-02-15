460b057c0d053ea1c3b9544f46c0d43da9d37dd2
JordanPenaloza <jordan332033@gmail.com>
2024-02-15 13:09:19 -0800

    TCP server created

---
+++ TCP/TCPServer.py
@@ -0,0 +1,16 @@
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+
+serverSocket.listen(1)
+print(" Server is running")
+
+while True:
+    connection, address = serverSocket.accept()
+    message = connection.recv(1024).decode()
+    modifiedMessage = message.upper()
+    
+    connection.send(modifiedMessage.encode())
+    connection.close()
