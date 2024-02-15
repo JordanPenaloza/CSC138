679a628a6377c76483f82190553939533006ffe6
JordanPenaloza <jordan332033@gmail.com>
2024-02-15 12:54:46 -0800

    created client

---
+++ TCP/TCPClient.py
@@ -0,0 +1,12 @@
+from socket import *
+
+serverName = 'localhost' # 127.0.0.1
+serverPort = 12000
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = input('Please enter your message in lowercase format: ')
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(1024)
+print("Upper case message: ", modifiedMessage.decode())
+
+clientSocket.close()
