7bb4fdacdd08cf1f1c968ea018e052d59ebe5511
JordanPenaloza <jordan332033@gmail.com>
2024-03-05 20:19:47 -0800

    created RTT calculator function

--- Assignment1/UDPClient.py
+++ Assignment1/UDPClient.py
@@ -1,4 +1,5 @@
 from socket import *
+import time
 
 server_address = ('127.0.0.1', 12000)
 client_socket = socket(AF_INET, SOCK_DGRAM)
@@ -7,8 +8,15 @@ client_socket.settimeout(1)
 ping_message = "Ping"
 num_pings = 10
 ping_attempt = 1
+rtt = 0
+
+def calculateRTT(end_time, start_time):
+    return (end_time - start_time) * 1000
 
 for ping in range(num_pings):
+
+    start_time = time.time()
+
     message_to_send = f"{ping_message} {ping_attempt}"
     try:
         client_socket.sendto(message_to_send.encode(), server_address)
@@ -17,9 +25,17 @@ for ping in range(num_pings):
         response, server_address = client_socket.recvfrom(1024)
         print(f"Received from {server_address}: {response.decode().upper()}")
 
+        end_time = time.time()
+        rtt = calculateRTT(end_time, start_time)
+        print(f"RTT: {rtt}")
+
     except:
         print("Request timed out")
     ping_attempt += 1
+
+
+
+
 
 
 client_socket.close()
