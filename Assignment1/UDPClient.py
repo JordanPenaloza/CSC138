c55b46bae9cd2b04815346fa82d480133bcb8b02
JordanPenaloza <jordan332033@gmail.com>
2024-03-07 13:56:42 -0800

    removed debugging print statements

--- Assignment1/UDPClient.py
+++ Assignment1/UDPClient.py
@@ -14,11 +14,10 @@ def calculateRTT(end_time, start_time):
 def sendMessage(ping_attempt):
     message_to_send = f"{ping_message} {ping_attempt}"
     client_socket.sendto(message_to_send.encode(), server_address)
-    print("Ping sent")
 
 def receiveMessage():
     response, server_address = client_socket.recvfrom(1024)
-    print(f"Received from {server_address}: {response.decode().upper()}")
+    print(f"{response.decode().upper()}")
 
 def main():
     for ping_attempt in range(1, num_pings + 1):
