fb612063feb51a72de33f7d303641308fd76ed90
JordanPenaloza <jordan332033@gmail.com>
2024-04-16 22:41:10 -0700

    added 2 graphs per assignment instructions and completed algorithm to find shortest path from all nodes, also added functions to ask the user which graph they want to use

---
+++ Assignment2/bellman_ford.py
@@ -0,0 +1,47 @@
+from graphs import graph1, graph2, graph3
+
+def bellman_ford(graph, source, num_nodes):
+    distances = [float("inf")] * (num_nodes + 1)
+    distances[source] = 0
+
+    for _ in range(num_nodes - 1):
+        for src, dest, cost in graph:
+            if distances[src] != float("inf") and distances[src] + cost < distances[dest]:
+                distances[dest] = distances[src] + cost
+            if distances[dest] != float("inf") and distances[dest] + cost < distances[src]:
+                distances[src] = distances[dest] + cost
+                
+    return distances[1:]
+
+def has_negative_cycle(distances):
+    for distance in distances:
+        if distance < 0:
+            return True
+    return False
+
+def choose_graph():
+    choice = input("Which graph would you like to run (1, 2 or 3): ")
+    if choice == "1":
+        graph = graph1
+    elif choice == "2":
+        graph = graph2
+    elif choice == "3":
+        graph = graph3
+    else:
+        print("Bad choice try again ")
+        choose_graph()
+    return graph
+
+def main():
+    graph = choose_graph()
+    num_nodes = max(max(node1, node2) for node1, node2, _ in graph)
+    for source in range(1, num_nodes + 1):
+        distances = bellman_ford(graph, source, num_nodes)
+        if has_negative_cycle(distances):
+                print(f"Negative weight cycle detected at node")
+                break
+        print(f"Shortest distance from Node {source}: {distances}")
+
+if __name__ == '__main__':
+    main()
+
